30c9b11588d5a05b7077d322d07afd646219f04d
fredericoqueiroz <frederico17@gmail.com>
2020-11-10 20:11:35 -0300

    configuracao para receber objeto

--- src/main/java/com/sandbox/sandboxkafkaconsumer/config/ConfigKafka.java
+++ src/main/java/com/sandbox/sandboxkafkaconsumer/config/ConfigKafka.java
@@ -3,6 +3,7 @@ package com.sandbox.sandboxkafkaconsumer.config;
 import com.sandbox.sandboxkafkaconsumer.model.Order;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,13 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class ConfigKafka {
+    @Value("${variables.bootstrap-servers-config}")
+    private String BOOTSTRAP_SERVERS_CONFIG;
+    @Value("${variables.group-id-config}")
+    private String GROUP_ID_CONFIG;
+    @Value("${variables.auto-offset-reset-config}")
+    private String AUTO_OFFSET_RESET_CONFIG;
+
 
     @Bean
     public ConsumerFactory<String, Order> consumerFactory(){
@@ -31,9 +39,9 @@ public class ConfigKafka {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID_CONFIG);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET_CONFIG);
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
